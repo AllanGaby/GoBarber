13b2c75b961aa6f31e57223d5abed7f57989139b
Allan Gaby <allan.gaby@gmail.com>
2020-07-04 16:44:28 -0300

    Remoção do Try/Catch na rota de sessão

--- server/src/routes/sessions.routes.ts
+++ server/src/routes/sessions.routes.ts
@@ -7,18 +7,11 @@ const sessionsRouter = Router();
 sessionsRouter.post('/', async (request, response) => {         
     const { email, password } = request.body;        
     const authenticateUser = new AuthenticateUserService();
-    try{
-        const { user, token } = await authenticateUser.execute({email, password});
-        return response.json({
-            user,
-            token
-        });
-    }
-    catch(err)
-    {
-        throw new AppError(err.message, err.statusCode);
-        console.log(err);
-    }
+    const { user, token } = await authenticateUser.execute({email, password});
+    return response.json({
+        user,
+        token
+    });
 });
 
 export default sessionsRouter;
